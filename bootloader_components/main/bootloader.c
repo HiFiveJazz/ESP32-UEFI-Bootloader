a78ecb371befdecc1db6f02eedc2deed97b63b6d
Jasmeet (Jazz) Bhatia <jasmeet.bhatia.us@gmail.com>
2026-09-12 03:18:19 -0700

    use clang, disable watchdog

--- bootloader_components/main/bootloader.c
+++ bootloader_components/main/bootloader.c
@@ -1,8 +1,16 @@
+#include "bootloader_utility.h"
+#include "bootloader_init.h"
 #include "esp_rom_sys.h"
 
 void __attribute__((noreturn)) call_start_cpu0(void)
 {
-    esp_rom_printf("Hello from Jazz BootLoader\n");
+    esp_rom_printf("Initializing Bootloader \n");
+
+    if (bootloader_init() != ESP_OK) {
+        bootloader_reset();
+    }
+
+    esp_rom_printf("Bootloader Initialized\n");
 
     while (1) {
     }
---
+++ bootloader_components/main/uefi.h
@@ -0,0 +1,6 @@
+struct simple_text_output {
+    /* function pointer goes here */
+};
+struct system_table {
+    struct simple_text_output *con_out;
+};

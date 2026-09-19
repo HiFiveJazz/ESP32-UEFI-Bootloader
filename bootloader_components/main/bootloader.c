8cdedbb854bf9d0f27146b149262af5cbd36e8c7
Jasmeet (Jazz) Bhatia <jasmeet.bhatia.us@gmail.com>
2026-09-18 21:32:32 -0700

    partition table working

--- bootloader_components/main/bootloader.c
+++ bootloader_components/main/bootloader.c
@@ -1,17 +1,29 @@
+#include "bootloader_config.h"
 #include "bootloader_utility.h"
 #include "bootloader_init.h"
 #include "esp_rom_sys.h"
 
 void __attribute__((noreturn)) call_start_cpu0(void)
 {
-    esp_rom_printf("Initializing Bootloader \n");
+	esp_rom_printf("Initializing Bootloader \n");
 
-    if (bootloader_init() != ESP_OK) {
-        bootloader_reset();
-    }
+	if (bootloader_init() != ESP_OK) {
+	bootloader_reset();
+	}
 
-    esp_rom_printf("Bootloader Initialized\n");
+	esp_rom_printf("Bootloader Initialized\n");
 
-    while (1) {
-    }
+	bootloader_state_t bs = {0};
+
+	esp_rom_printf("Loading partition table ...\n");
+
+	if (!bootloader_utility_load_partition_table(&bs)) {
+		esp_rom_printf("Failed to load partition table\n");
+		bootloader_reset();
+	}
+
+	esp_rom_printf("Partition table loaded successfully\n");
+
+	while (1) {
+	}
 }
